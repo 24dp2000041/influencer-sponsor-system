cbce3dca0f9e84f50ce617332ca0deed53e2ccb1
24dp2000041 <24dp2000041@ds.study.iitm.ac.in>
2024-07-26 20:59:24 +0530

    update on 26-07-2024 21:00

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ import sqlite3
 from flask import g
 
 def connect_to_Database():
-    sql = sqlite3.connect("C:\Users\user\Documents\GitHub\influencer-sponsor-system\employee.db")
+    sql = sqlite3.connect("C:\Users\user\influencer-sponsor-system\employee.db")
     sql.row_factory = sqlite3.Row
     return sql
 
-def get_databases():
+def get_database():
     if not hasattr(g, "employee_db"):
         g.employee_db = connect_to_Database()
     return g.employee_db
